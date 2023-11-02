9321d6e159ce9cf496ad3d243bfd70b96388f0b8
Barbara-dio <tavaresbarbara97@gmail.com>
2023-11-02 18:41:02 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-dsad
+let nomeHeroi = "Ângelito"
+let xpHeroi = 1000
+let nivel = ""
 
-aijsiJASIJa veraO 0.0.1
+if (xpHeroi <= 1000) {nivel = "Ferro"}
+    else if (xpHeroi >= 1001 && xpHeroi <= 2000) {nivel = "Bronze"}
+    else if (xpHeroi >= 2001 && xpHeroi <= 5000) {nivel = "Prata"}
+    else if (xpHeroi >= 6001 && xpHeroi <= 7000) {nivel = "Ouro"}
+    else if (xpHeroi >= 7001 && xpHeroi <= 8000) {nivel = "Platina"}
+    else if (xpHeroi >= 8001 && xpHeroi <= 9000) {nivel = "Ascendente"}
+    else if (xpHeroi >= 9001 && xpHeroi <= 10000) {nivel = "Imortal"}
+        else (xpHeroi >= 10001);
 
-hhihjhkjhj
-
-djjdosjkj
+console.log ("O heroi " + nomeHeroi, "está no nível " + nivel)
